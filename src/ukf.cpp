29756784f923e497c8a47f19b6f4867bbd3b944c
Steven Eisinger <steven.a.eisinger@gmail.com>
2017-11-06 23:23:35 -0500

    Fixed it!

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -27,10 +27,10 @@ UKF::UKF() {
   lambda_ = 3 - n_aug_;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 3;
+  std_a_ = 0.2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.75;
+  std_yawdd_ = 0.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -149,8 +149,8 @@ void UKF::Prediction(double delta_t) {  // FIXME: Xsig_pred_(3) grows v. large
   
   //create augmented mean state
   x_aug.head(n_x_) = x_;
-  x_aug(n_aug_ - 2) = 0;
-  x_aug(n_aug_ - 1) = 0;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
 
   //create augmented covariance matrix
   P_aug.fill(0.0);
@@ -165,7 +165,7 @@ void UKF::Prediction(double delta_t) {  // FIXME: Xsig_pred_(3) grows v. large
   Xsig_aug.col(0)  = x_aug;
   for (int i = 0; i< n_aug_; i++)
   {
-    Xsig_aug.col(i+1)       = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
+    Xsig_aug.col(i+1)        = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
     Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
   }
 
@@ -215,11 +215,13 @@ void UKF::Prediction(double delta_t) {  // FIXME: Xsig_pred_(3) grows v. large
   }
 
   //predicted state mean x_
+  x_.fill(0.0);
   for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
     x_ = x_ + weights_(i) * Xsig_pred_.col(i);
   }
   
   //predicted state covariance matrix P_
+  P_.fill(0.0);
   for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
 
     // state difference
